0857c77528520d7714860823614ec9cf7cebbf05
wenhaoqiasd <wenhaoqi@126.com>
2020-03-24 18:16:49 +0800

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <section>
-      <h1 className="title text-center" style={{ margin: '8rem 0 0' }}>404</h1>
+      <h1 className="title text-center" style={{ margin: '8rem 0 0' }}>404: Not found</h1>
       <h1 className="text-center">Oops! That page can’t be found.</h1>
     </section>
   </Layout>
