e0a72fdba659b8cc67280ab5e5e348f7b490fbe1
wenhaoqi <wenhaoqi@126.com>
2020-06-19 12:31:37 +0800

    回滚一下

--- src/components/banner.js
+++ src/components/banner.js
@@ -37,47 +37,8 @@ const Banner = () => (
       </div>
     </a>
 
-<<<<<<< HEAD
-  return (
-    <>
-      {data.sandwiches.nodes.map((item) => (
-          <a
-            key={item.recordId}
-            className="banner-space"
-            style={{ backgroundColor: item.data.Color }}
-            href={item.data.BannerLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Tilt
-              className="Tilt"
-              options={{
-                max: 20,
-                reverse: true,
-                scale: 1.04,
-                reset: true,
-                perspective: 1600,
-              }}
-            >
-              <img
-                // src={item.data.Cover[0].url}
-                alt={item.data.Name}
-                className="Tilt-inner"
-              />
-            </Tilt>
-            <div className="banner-ring">
-              <p>{item.data.Description}</p>
-              <BannerRing />
-            </div>
-          </a>
-      ))}
-    </>
-  );
-};
-=======
     <BannerSvg />
   </>
 );
->>>>>>> parent of eb00468... vx7
 
 export default Banner;
