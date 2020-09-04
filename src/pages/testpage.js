f50baeb89d06b52526010362e7cfe2926a597f1c
wenhaoqi <wenhaoqi@126.com>
2020-09-04 15:34:28 +0800

    10.0.5.1
    增加threejs

--- src/pages/testpage.js
+++ src/pages/testpage.js
@@ -1,16 +1,117 @@
-import React from "react";
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from 'react-three-fiber'
 
-import SEO from "../components/seo";
-import PageUp from "../components/page-up";
+import "./testpage.css"
 
-import "./testpage.css";
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  // useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+  useFrame(() => (mesh.current.rotation.x += 0.01))
+  useFrame(() => (mesh.current.rotation.y += 0.02))
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [0.7, 0.7, 0.7] : [0.5, 0.5, 0.5]}
+      onClick={(e) => setActive(!active)}
+      onPointerOver={(e) => setHover(true)}
+      onPointerOut={(e) => setHover(false)}>
+      <boxBufferGeometry attach="geometry" args={[0.5, 0.5, 0.5]} />
+      <meshStandardMaterial attach="material" color={hovered ? 'yellow' : 'orange'} />
+    </mesh>
+  )
+}
+
+function Circle(props) {
+  const mesh = useRef()
+  useFrame(() => (mesh.current.rotation.x += 0.01))
+  useFrame(() => (mesh.current.rotation.y += 0.02))
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      >
+      <tetrahedronBufferGeometry attach="geometry" args={[0.25]}/>
+      <meshStandardMaterial attach="material" color={"hotpink"} />
+    </mesh>
+  )
+}
+
+function Torus(props) {
+  const mesh = useRef()
+  useFrame(() => (mesh.current.rotation.x -= 0.02))
+  useFrame(() => (mesh.current.rotation.y -= 0.01))
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      >
+      <torusBufferGeometry attach="geometry" args={[0.15, 0.05, 0.05 , 100]} />
+      <meshStandardMaterial attach="material" color={"aqua"} />
+    </mesh>
+  )
+}
+
+export default function App() {
+  return (
+    <div className="view">
+      <Canvas colorManagement camera={{ position: [0, 0, 24], fov: 20 }}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        
+        <Box position={[-2.5, 2.5, 0]} />
+        <Circle position={[-1.5, 2.5, 0]} />
+        <Torus position={[-0.5, 2.5, 0]} />
+        <Box position={[0.5, 2.5, 0]} />
+        <Circle position={[1.5, 2.5, 0]} />
+        <Torus position={[2.5, 2.5, 0]} />
+        {/* --- */}
+        <Circle position={[-2.5, 1.5, 0]} />
+        <Torus position={[-1.5, 1.5, 0]} />
+        <Box position={[-0.5, 1.5, 0]} />
+        <Circle position={[0.5, 1.5, 0]} />
+        <Torus position={[1.5, 1.5, 0]} />
+        <Box position={[2.5, 1.5, 0]} />
+        {/* --- */}
+        <Torus position={[-2.5, 0.5, 0]} />
+        <Box position={[-1.5, 0.5, 0]} />
+        <Circle position={[-0.5, 0.5, 0]} />
+        <Torus position={[0.5, 0.5, 0]} />
+        <Box position={[1.5, 0.5, 0]} />
+        <Circle position={[2.5, 0.5, 0]} />
+        {/* --- */}
+
+        <Box position={[-2.5, -0.5, 0]} />
+        <Circle position={[-1.5, -0.5, 0]} />
+        <Torus position={[-0.5, -0.5, 0]} />
+        <Box position={[0.5, -0.5, 0]} />
+        <Circle position={[1.5, -0.5, 0]} />
+        <Torus position={[2.5, -0.5, 0]} />
+        {/* --- */}
+        <Circle position={[-2.5, -1.5, 0]} />
+        <Torus position={[-1.5, -1.5, 0]} />
+        <Box position={[-0.5, -1.5, 0]} />
+        <Circle position={[0.5, -1.5, 0]} />
+        <Torus position={[1.5, -1.5, 0]} />
+        <Box position={[2.5, -1.5, 0]} />
+        {/* --- */}
+        <Torus position={[-2.5, -2.5, 0]} />
+        <Box position={[-1.5, -2.5, 0]} />
+        <Circle position={[-0.5, -2.5, 0]} />
+        <Torus position={[0.5, -2.5, 0]} />
+        <Box position={[1.5, -2.5, 0]} />
+        <Circle position={[2.5, -2.5, 0]} />
 
 
-const TextPage = () => (
-  <main className="view">
-    <SEO title="test" />
-    <PageUp />
-  </main>
-);
-
-export default TextPage;
+      </Canvas>
+    </div>
+  )
+}
